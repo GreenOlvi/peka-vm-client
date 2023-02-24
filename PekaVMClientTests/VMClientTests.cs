54ac05abfc873773ecede422061b10f4f938e28b
Piotr Szulc <szulo123@gmail.com>
2023-02-24 15:57:33 +0100

    Removed test app and added proper test project

---
+++ PekaVMClientTests/VMClientTests.cs
@@ -0,0 +1,15 @@
+namespace PekaVMClientTests;
+
+public class VMClientTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
